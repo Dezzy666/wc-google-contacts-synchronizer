e6a2faf19b013d97c994a51a867e44d8d7edeefb
Jan Herzan <jan.herzan@gmail.com>
2017-04-30 20:06:49 +0200

    Project init

---
+++ wc-google-contacts-synchronizer.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Woocommerce google contacts synchronizer
+Plugin URI:  https://developer.wordpress.org/plugins/wc-google-contacts-sycnhornizer/
+Description: Plugin for symchronizing your woocommerce accounts with google contacts.
+Version:     1.0.0
+Author:      Jan Herzan
+Author URI:  https://jan.herzan.eu/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wc_google_contacts_synchronizer
+Domain Path: /languages
+*/
